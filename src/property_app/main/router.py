3e52bc649a9eee38da3cd7272e8e26171a2d02f6
amcclosky <anthony@mcclosky.dev>
2020-05-10 11:46:22 -0500

    add an asset_url helper

--- src/property_app/main/router.py
+++ src/property_app/main/router.py
@@ -1,12 +1,39 @@
 import os
+import pathlib
+import json
+
+import jinja2
 
 from starlette.applications import Starlette
 from starlette.routing import Router
 from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
 
+
+@jinja2.contextfunction
+def asset_url(context: dict, name: str):
+    manifest = None
+    app_dir = pathlib.Path(local_dir).parent
+    manifest_path = app_dir / pathlib.Path("static/dist/manifest.json")
+    with open(manifest_path, "rb") as manifest_file:
+        manifest = json.load(manifest_file)
+
+    dist_dir = pathlib.Path("dist")
+    asset_name = pathlib.Path(manifest[name])
+
+    asset_path = dist_dir / asset_name
+
+    request = context["request"]
+
+    return request.url_for("static", path=asset_path.as_posix())
+
+
 local_dir = os.path.dirname(os.path.abspath(__file__))
 templates = Jinja2Templates(directory=os.path.join(local_dir, "templates"))
+
+jinja_env = templates.env
+jinja_env.globals["asset_url"] = asset_url
+
 
 main = Router()
 
