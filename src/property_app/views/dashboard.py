aba85f2764c2b323529cd744a94fb657fc83bcd0
amcclosky <anthony@mcclosky.dev>
2020-05-12 23:44:25 -0500

    try the requires decorator

--- src/property_app/views/dashboard.py
+++ src/property_app/views/dashboard.py
@@ -1,6 +1,9 @@
+from starlette.authentication import requires
+
 from property_app.lib.inertia import InertiaRequest, InertiaHTTPEndpoint
 
 
 class Dashboard(InertiaHTTPEndpoint):
+    @requires(["authenticated"], redirect="Index")
     def get(self, request: InertiaRequest):
         return {"userProfile": {"username": request.user.username}}
