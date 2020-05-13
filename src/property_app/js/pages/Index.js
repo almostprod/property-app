eed3402cf47d4d5da2784cf5e5099ad339c07b7b
amcclosky <anthony@mcclosky.dev>
2020-05-12 23:38:59 -0500

    add austin sunset unsplash

--- src/property_app/js/pages/Index.js
+++ src/property_app/js/pages/Index.js
@@ -95,7 +95,7 @@ const Index = () => (
     <div className="hidden lg:block relative w-0 flex-1">
       <img
         className="absolute inset-0 h-full w-full object-cover"
-        src="https://images.unsplash.com/photo-1505904267569-f02eaeb45a4c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1908&q=80"
+        src="https://images.unsplash.com/photo-1513205352338-335d8aee3f75?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1908&q=80"
         alt=""
       />
     </div>
