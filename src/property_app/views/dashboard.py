261527748234bb49a2f9ea2506a22e69e07a1393
amcclosky <anthony@mcclosky.dev>
2020-05-13 19:12:59 -0500

    cleanup

--- src/property_app/views/dashboard.py
+++ src/property_app/views/dashboard.py
@@ -6,8 +6,10 @@ from property_app.lib.inertia import InertiaRequest, InertiaHTTPEndpoint
 class Dashboard(InertiaHTTPEndpoint):
     @requires(["authenticated"], redirect="Index")
     def get(self, request: InertiaRequest):
+        user = request.user
+
         return {
-            "userProfile": {"username": request.user.username},
+            "userProfile": {"username": user.username},
             "indexUrl": request.url_for("Index"),
             "signUpUrl": request.url_for("SignUp"),
             "signOutUrl": request.url_for("SignOut"),
